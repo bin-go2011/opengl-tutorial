14b0748ab42cc61744bf4c751de450359769d273
bji <ben.ji@polycom.com>
2018-08-07 12:25:57 +0800

    01 hello_freeglut

---
+++ 01_hello_freeglut/LOpenGL.h
@@ -0,0 +1,13 @@
+/*This source code copyrighted by Lazy Foo' Productions (2004-2013)
+and may not be redistributed without written permission.*/
+//Version: 001
+
+#ifndef LOPENGL_H
+#define LOPENGL_H
+
+#include <GL/freeglut.h>
+#include <gl.h>
+#include <glu.h>
+#include <stdio.h>
+
+#endif
